daa48e275ebb2fbfb9b40359737c94f83d0eb4e7
Jenna Francis <jef399@nyu.edu>
2017-04-06 14:41:51 +0000

    locations controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,12 @@
 class LocationsController < ApplicationController
+
+  get '/locations' do
+    @locations = Location.all
+    erb :'locations/index'
+  end
+
+  get '/locations/:slug' do
+    @location = Location.find_by_slug(params[:slug])
+    erb :'locations/show'
+  end
 end
