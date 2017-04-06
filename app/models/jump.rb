4f140f2f889e9380025e2c6e8747c38b031085e1
Jenna Francis <jef399@nyu.edu>
2017-04-06 00:32:07 +0000

    add validation to Jump and Location class, slugify locations

--- app/models/jump.rb
+++ app/models/jump.rb
@@ -1,4 +1,5 @@
 class Jump < ActiveRecord::Base
   belongs_to :user
   belongs_to :location
+  validates_presence_of :user_id, :location_id, :kind, :height
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,13 @@
 class Location < ActiveRecord::Base
   has_many :jumps
   has_many :users, through: :jumps
+  validates_presence_of :name, :address
+
+  def slug
+    self.username.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.detect{|item| item.slug == slug}
+  end
 end
