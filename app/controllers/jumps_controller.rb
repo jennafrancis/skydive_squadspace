97af4962f26c9c0598d62aee85c8bb0bd5786ede
Jenna Francis <jef399@nyu.edu>
2017-04-06 20:13:47 +0000

    show jump

--- app/controllers/jumps_controller.rb
+++ app/controllers/jumps_controller.rb
@@ -14,6 +14,24 @@ class JumpsController < ApplicationController
     # redirect to user profile
   end
 
-  
+  get '/jumps/:id' do
+    @jump = Jump.find(params[:id])
+    erb :'jumps/show'
+  end
+
+  get '/jumps/:id/edit' do
+
+    erb :'jumps/edit'
+  end
+
+  delete '/jumps/:id' do
+  @jump = Jump.find(params[:id])
+  if is_logged_in? && current_user == @jump.user
+    @jump.destroy
+  end
+  #error message
+  redirect '/jumps'
+end
+
 
 end
