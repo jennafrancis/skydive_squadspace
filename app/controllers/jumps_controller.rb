66a0add0567e89dbd6aa253834325c95f0700b20
Jenna Francis <jef399@nyu.edu>
2017-04-06 15:32:43 +0000

    jumps controller

--- app/controllers/jumps_controller.rb
+++ app/controllers/jumps_controller.rb
@@ -1,2 +1,19 @@
 class JumpsController < ApplicationController
+
+  get '/jumps' do
+    @jumps = Jump.all
+    erb :'jumps/index'
+  end
+
+  get '/jumps/new' do
+    erb :'jumps/new'
+  end
+
+  post '/jumps' do
+
+    # redirect to user profile
+  end
+
+  
+
 end
