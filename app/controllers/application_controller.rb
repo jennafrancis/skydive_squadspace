1c8f9a8d14dc330a77f501c9d8d0dce0a2421a43
Jenna Francis <jef399@nyu.edu>
2017-04-07 17:21:44 +0000

    flash messages working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,12 @@ require 'rack-flash'
 class ApplicationController < Sinatra::Base
 
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-    enable :sessions
-    set :session_secret, "secret"
-  end
-
-  use Rack::Flash
+      set :public_folder, 'public'
+      set :views, 'app/views'
+      enable :sessions
+      set :session_secret, "secret"
+      use Rack::Flash, sweep: true
+    end
 
   get '/' do
     erb :index
